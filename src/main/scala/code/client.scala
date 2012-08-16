7a348f425c4ad24d3ce1cb33e82ad669550326d9
Chris Baxter <chris@aquto.com>
2012-08-16 13:59:42 -0400

    Checking in final content for presentation

--- src/main/scala/code/client.scala
+++ src/main/scala/code/client.scala
@@ -3,6 +3,7 @@ import scala.concurrent._
 import scala.util._
 import java.util.concurrent.Executors
 import java.sql.SQLException
+import util.duration._
 
 package object code{
    implicit val execctx = ExecutionContext.fromExecutor(Executors.newCachedThreadPool()) 
@@ -10,10 +11,21 @@ package object code{
 
 object Client{
   import code._
+  val priceService = new PriceService
   
   def main(args: Array[String]) {
+    basic
+    nested
+    map
+    await
+    forcomp
+    recover
+    fallback
+    trysuccess
+  }
   
-    /*val f:Future[Int] = future{
+  def basic() = {
+    val f:Future[Int] = future{
       WaitService.echo(2)
     }
     
@@ -37,20 +49,21 @@ object Client{
     fut onFailure {
       case npe:NullPointerException => 
         println("Never going to get here")    
-    }
-    */
-    val priceService = new PriceService
+    }    
+  }
+  
+  def nested = {
     val productId1 = 1
     val productId2 = 1
     
-    /*val priceCheck = future{
-      priceService.checkPrice(productId)
+    val priceCheck = future{
+      priceService.checkPrice(productId1)
     }
     
     priceCheck onSuccess {
       case price =>
         val purchase = future{
-          if (price < 100) priceService.purchaseItem(productId)
+          if (price < 100) priceService.purchaseItem(productId1)
           else throw new Exception("to rich for my blood")
         }
         
@@ -58,35 +71,94 @@ object Client{
           case bool => 
             println("Purchase was a success")
         }
-    }
-    */
-    /*val priceCheck = future{
-      priceService.checkPrice(productId)
+    }    
+  }
+  
+  def map = {
+    val priceCheck = future{
+      priceService.checkPrice(123)
     }
     
     val purchase = priceCheck map {
       price =>
-        if (price < 100) priceService.purchaseItem(productId)
+        if (price < 100) priceService.purchaseItem(123)
         else throw new Exception("to rich for my blood")
-    }    
-
+    }   
+    
     purchase onSuccess{
       case bool => 
         println("Purchase was a success")
-    } */
-    
-    val prod1 = future{priceService.checkPrice(productId1)}
-    val prod2 = future{priceService.checkPrice(productId2)}
+    }     
+  }
+  
+  def await = {
+    val purchase = future {
+      priceService.checkPrice(123)
+    }
+    val result = Await.result(purchase, 1 second)
+  }
+  
+  def forcomp = {
+    val prod1 = future{priceService.checkPrice(123)}
+    val prod2 = future{priceService.checkPrice(456)}
     
     val purchase = for{
       price1 <- prod1
       price2 <- prod2
       if (price1 + price2 < 200)
-    } yield priceService.purchaseBoth(productId1, productId2)
+    } yield priceService.purchaseBoth(123, 456)
     
     purchase onSuccess{
       case bool => 
         println("Purchase was a success")
-    }    
+    }     
   }
+  
+  def recover = {
+    val prod = future{priceService.checkPrice(123)}
+    val purchase = prod map{
+      price => priceService.purchaseItem(999)
+    } recover {
+      case ne:NotAvailableException => false
+    }  
+    
+    purchase onSuccess {
+      case b => println("Got value: " + b)
+    }
+  }
+  
+  def fallback = {
+    val prod1Res = future{
+      priceService.checkPrice(123)
+    } map{
+      price => "Product 1 price: " + price
+    }
+    
+    val prod2Res = future{
+      priceService.checkPrice(456)
+    } map{
+      price => "Product 2 price: " + price
+    }   
+    
+    val result = prod1Res fallbackTo prod2Res
+    result onSuccess {case str => println(str)}    
+  }
+  
+  def trysuccess = {
+    val p = Promise[Int]
+    val pf = p.future
+    val fut1 = future{
+      val i = someLongRunningIntOp()
+      p trySuccess i
+    }
+    val fut2 = future{
+      val i = someOtherLongRunningIntOp()
+      p trySuccess i
+    }
+    
+    pf onSuccess { case i => println("Got value: " + i)}    
+  }
+  
+  def someLongRunningIntOp() = {1}
+  def someOtherLongRunningIntOp() = {2}
 }
--- src/main/scala/code/services.scala
+++ src/main/scala/code/services.scala
@@ -1,4 +1,5 @@
 package code
+import java.util.Random
 
 object WaitService{
   def echo(i:Int):Int = {
@@ -8,15 +9,25 @@ object WaitService{
 }
 
 class PriceService{
+  val r = new Random(System.currentTimeMillis)
+  
   def checkPrice(productId:Long):Double = {
-    90
+    Thread.sleep(r.nextInt(1000))
+    if (productId == 123) 25.99
+    else if (productId == 456) 35.99
+    else 99.99
   }
   
   def purchaseItem(productId:Long):Boolean = {
+    Thread.sleep(r.nextInt(1000))
+    if (productId == 999) throw new NotAvailableException
     true
   }
   
   def purchaseBoth(product1:Long, product2:Long):Boolean ={
+    Thread.sleep(r.nextInt(1000))
     true
   }
 }
+
+class NotAvailableException extends Exception
